4992caf27ea2b0b034dcaa531a4a7deaf796810d
devsharma2208 <devsharmaelc@gmail.com>
2023-08-19 10:32:02 +0530

    math partner project

--- App.js
+++ App.js
@@ -1,1 +1,75 @@
+const searchBtn = document.querySelector(".searchBtn");
+const input = document.querySelector("#inputVal");
+let result = document.querySelector("#result");
+const mathscatagery = document.querySelector("#mathsCategory");
 
+let history_arr = [];
+
+async function getData(mathscatageryValue, encodedExpression) {
+    const response = await fetch(`https://newton.now.sh/api/v2/${mathscatageryValue}/${encodedExpression}`)
+    const json = await response.json();
+
+    create_element(json);
+}
+
+searchBtn.addEventListener('click', () => {
+    if (input.value.trim() !== '') {
+
+        let mathscatageryValue = mathscatagery.value;
+        const InputExpression = input.value;
+        const encodedExpression = encodeURI(InputExpression);
+
+        getData(mathscatageryValue, encodedExpression);
+
+    }
+    else {
+        alert("Enter some value....")
+    }
+})
+
+function create_element(json) {
+    let div = document.querySelector("#show_div")
+    div.classList.add("completeResult");
+
+    let h2 = div.querySelector("h2");
+    h2.classList.add("selected-operation");
+    h2.innerText = `${json.operation} : ${json.expression}`;
+
+    let showResult = div.querySelector("p");
+    showResult.classList.add("find-result");
+    showResult.innerText = json.result;
+
+    let deleteBtn = div.querySelector("i");
+    deleteBtn.classList.add("fa-regular", "fa-trash-can", "trase");
+
+    history_arr.push(json);
+
+    localStorage.setItem('data', JSON.stringify(history_arr));
+
+    input.value = "";
+}
+function deleteData(event) {
+    let div = document.querySelector("#show_div")
+    div.classList.remove("completeResult");
+    let h2 = div.querySelector("h2");
+    h2.classList.remove("selected-operation")
+    h2.innerText = '';
+    let p = div.querySelector("p")
+    p.classList.remove("find-result")
+    p.innerText = '';
+    let i = div.querySelector("i");
+    i.classList.remove("fa-regular", "fa-trash-can", "trase")
+
+    history_arr = JSON.parse(localStorage.getItem('data'));
+
+    if (Array.isArray(history_arr)){
+        history_arr.pop();
+        localStorage.clear();
+        localStorage.setItem("data", JSON.stringify(history_arr));
+    }
+    // else if(history_arr.length === 0){
+    //     localStorage.clear();
+    // }
+}
+
+history_arr = JSON.parse(localStorage.getItem('data'));
---
+++ solutions.js
@@ -0,0 +1,47 @@
+history_arr = JSON.parse(localStorage.getItem('data'));
+
+let section = document.querySelector("#result");
+let count = 0;
+if (history_arr.length !== 0){
+    for (let i = 0; i < history_arr.length; i++) {
+
+        let div = document.createElement("div");
+        div.classList.add("completeResult");
+        div.classList.add(i);
+        let h2 = document.createElement("h2");
+        h2.classList.add("selected-operation");
+        h2.innerText = `${history_arr[i].operation} : ${history_arr[i].expression}`;
+
+        let showResult = document.createElement("p");
+        showResult.classList.add("find-result");
+        showResult.innerText = history_arr[i].result;
+        count++;
+        let deleteBtn = document.createElement("i");
+        deleteBtn.classList.add("fa-regular", "fa-trash-can", "trase")
+        deleteBtn.setAttribute("id", i);
+
+        deleteBtn.onclick=function(){
+            deleteData(i);
+            location.reload();
+        }
+
+        div.appendChild(h2);
+        div.appendChild(showResult);
+        div.appendChild(deleteBtn);
+        section.appendChild(div);
+    }
+}
+
+function deleteData(i) {
+    history_arr = JSON.parse(localStorage.getItem('data'));
+
+    if (Array.isArray(history_arr)){
+        history_arr.splice(parseInt(i), 1);
+        localStorage.clear();
+        localStorage.setItem("data", JSON.stringify(history_arr));
+    }
+    // else if(history_arr.length === 0){
+    //     localStorage.clear();
+    // }
+}
+history_arr = JSON.parse(localStorage.getItem('data'));
